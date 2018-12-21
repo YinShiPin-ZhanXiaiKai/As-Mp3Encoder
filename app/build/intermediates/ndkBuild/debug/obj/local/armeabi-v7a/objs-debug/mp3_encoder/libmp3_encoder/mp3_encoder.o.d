386052daa06640fb24ea1ed2f68169d3485bf9bc
LiuYi <1017965668>
2018-12-21 10:22:06 +0800

    修复init未定义返回值的bug

--- app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/mp3_encoder/libmp3_encoder/mp3_encoder.o.d
+++ app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/mp3_encoder/libmp3_encoder/mp3_encoder.o.d
@@ -1,8 +1,8 @@
-/Users/mac/Documents/githubCode/As-Mp3Encoder/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/mp3_encoder/./libmp3_encoder/mp3_encoder.o: \
- /Users/mac/Documents/githubCode/As-Mp3Encoder/app/src/main/jni/mp3_encoder/./libmp3_encoder/mp3_encoder.cpp \
- /Users/mac/Documents/githubCode/As-Mp3Encoder/app/src/main/jni/mp3_encoder/./libmp3_encoder/mp3_encoder.h \
- /Users/mac/Documents/githubCode/As-Mp3Encoder/app/src/main/jni/mp3_encoder/../3rdparty/lame/include/lame/lame.h
+/Users/mac/Documents/githubCode/zhanxiaokai/As-Mp3Encoder/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/mp3_encoder/./libmp3_encoder/mp3_encoder.o: \
+ /Users/mac/Documents/githubCode/zhanxiaokai/As-Mp3Encoder/app/src/main/jni/mp3_encoder/./libmp3_encoder/mp3_encoder.cpp \
+ /Users/mac/Documents/githubCode/zhanxiaokai/As-Mp3Encoder/app/src/main/jni/mp3_encoder/./libmp3_encoder/mp3_encoder.h \
+ /Users/mac/Documents/githubCode/zhanxiaokai/As-Mp3Encoder/app/src/main/jni/mp3_encoder/../3rdparty/lame/include/lame/lame.h
 
-/Users/mac/Documents/githubCode/As-Mp3Encoder/app/src/main/jni/mp3_encoder/./libmp3_encoder/mp3_encoder.h:
+/Users/mac/Documents/githubCode/zhanxiaokai/As-Mp3Encoder/app/src/main/jni/mp3_encoder/./libmp3_encoder/mp3_encoder.h:
 
-/Users/mac/Documents/githubCode/As-Mp3Encoder/app/src/main/jni/mp3_encoder/../3rdparty/lame/include/lame/lame.h:
+/Users/mac/Documents/githubCode/zhanxiaokai/As-Mp3Encoder/app/src/main/jni/mp3_encoder/../3rdparty/lame/include/lame/lame.h:
--- app/src/main/jni/mp3_encoder/jni/AudioEncoder.cpp
+++ app/src/main/jni/mp3_encoder/jni/AudioEncoder.cpp
@@ -11,11 +11,12 @@ JNIEXPORT jint JNICALL Java_com_phuket_tour_studio_Mp3Encoder_init
 			jint sampleRate, jstring mp3PathParam) {
 	const char* pcmPath = env->GetStringUTFChars(pcmPathParam, NULL);
 	const char* mp3Path = env->GetStringUTFChars(mp3PathParam, NULL);
-	LOGI("mp3Path is %s...", mp3Path);
+	LOGI("mp3Path= %s，pcmPath=%s", mp3Path,pcmPath);
 	encoder = new Mp3Encoder();
-	encoder->Init(pcmPath, mp3Path, sampleRate, channels, bitRate);
+	jint result =encoder->Init(pcmPath, mp3Path, sampleRate, channels, bitRate);
 	env->ReleaseStringUTFChars(mp3PathParam, mp3Path);
 	env->ReleaseStringUTFChars(pcmPathParam, pcmPath);
+	return result;
 }
 
 JNIEXPORT void JNICALL Java_com_phuket_tour_studio_Mp3Encoder_encode(JNIEnv * env, jobject obj) {
